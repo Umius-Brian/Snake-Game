5103896384f33e3ea8f0e823f9cf95644529ce19
Wenfei Zhou <wzhou2046@gmail.com>
2020-03-24 16:08:16 -0700

    added gameover functionality

--- src/Head.js
+++ src/Head.js
@@ -1,8 +1,7 @@
 class Head {
-
   // this is what's called when you use the "new" keyword
   constructor($el) {
-    this.currentDirection = 'right';
+    this.currentDirection = "right";
     this.SPEED = 150;
     this.positionTop = 0;
     this.positionLeft = 0;
@@ -16,41 +15,47 @@ class Head {
     let direction = this.currentDirection;
     // console.log('this111111', this)
 
-    if (direction === 'right') {
+    if (direction === "right") {
       this.positionLeft += 50;
     }
 
-    if (direction === 'left') {
+    if (direction === "left") {
       this.positionLeft -= 50;
     }
-    if (direction === 'up') {
+    if (direction === "up") {
       this.positionTop -= 50;
     }
-    if (direction === 'down') {
+    if (direction === "down") {
       this.positionTop += 50;
     }
 
-    if (this.positionLeft < 0 || this.positionTop < 0 || this.positionLeft > 650 || this.positionTop > 650) {
-      window.alert("AND SO ARE YOU!");
+    if (
+      this.positionLeft < 0 ||
+      this.positionTop < 0 ||
+      this.positionLeft > 650 ||
+      this.positionTop > 650
+    ) {
+      this.gameOver();
       this.positionLeft = 0;
       this.positionTop = 0;
-      gameLoop();
     }
 
     let dead = false;
     for (let i = 0; i < body.nodes.length - 1; i++) {
-      if (body.nodes[i].positionLeft === this.positionLeft && body.nodes[i].positionTop === this.positionTop) {
+      if (
+        body.nodes[i].positionLeft === this.positionLeft &&
+        body.nodes[i].positionTop === this.positionTop
+      ) {
         dead = true;
       }
     }
     if (dead) {
-      alert("AW I'D RATHER BE PLAYING TETRIS");
+      this.gameOver();
       this.positionLeft = 0;
       this.positionTop = 0;
-      gameLoop();
     }
 
-    // experiment with deletion 
+    // experiment with deletion
     let position = this.node.position();
     position.left = this.positionLeft;
     position.top = this.positionTop;
@@ -61,7 +66,17 @@ class Head {
   }
 
   getCurrentPosition() {
-    return [this.positionLeft, this.positionTop]
+    return [this.positionLeft, this.positionTop];
+  }
+
+  gameOver() {
+    let replay = confirm("SO ARE YOU! Try again?");
+    if (replay == true) {
+      location.reload(true);
+    } else {
+      window.alert("DUN GOOFED");
+      window.open("https://coronavirus.jhu.edu/map.html");
+    }
   }
 }
 
@@ -74,9 +89,3 @@ HOW:
 if the snake is at position Top/Left < 0 OR at position Top/Left > 650
 then alert GAME OVER and show refresh
 */
-
-
-
-
-
-
