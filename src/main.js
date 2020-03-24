4efc6f5ea0a69a7ba708e05b3b6f6b518dae918c
Wenfei Zhou <wzhou2046@gmail.com>
2020-03-24 15:39:59 -0700

    wenfei commit

--- src/main.js
+++ src/main.js
@@ -1,72 +1,71 @@
 $(document).ready(function() {
-  const head = new Head($('#board'));
-  let apple = new Apple($('#board'));
-  let body = new Body($('#board'));
+  const head = new Head($("#board"));
+  let apple = new Apple($("#board"));
+  let body = new Body($("#board"));
 
   let keyDownEvents = [];
-  $('body').on('keydown', function(e) {
+  $("body").on("keydown", function(e) {
     keyDownEvents.push(e);
   });
 
   function processInputs() {
     for (let i = 0; i < keyDownEvents.length; i++) {
       let e = keyDownEvents[i];
-      if (e.keyCode === 37 && head.currentDirection !== 'right') {
-        head.currentDirection = 'left';
+      if (e.keyCode === 37 && head.currentDirection !== "right") {
+        head.currentDirection = "left";
       }
-  
-      if (e.keyCode === 38 && head.currentDirection !== 'down') {
-        head.currentDirection = 'up';
+
+      if (e.keyCode === 38 && head.currentDirection !== "down") {
+        head.currentDirection = "up";
       }
-  
-      if (e.keyCode === 39 && head.currentDirection !== 'left') {
-        head.currentDirection = 'right';
+
+      if (e.keyCode === 39 && head.currentDirection !== "left") {
+        head.currentDirection = "right";
       }
-  
-      if (e.keyCode === 40 && head.currentDirection !== 'up') {
-        head.currentDirection = 'down';
+
+      if (e.keyCode === 40 && head.currentDirection !== "up") {
+        head.currentDirection = "down";
       }
     }
     keyDownEvents = [];
   }
 
-    // Check if the snake is eating the apple every head.SPEED ms
-    function update() {
-      let headLastPosition = head.getCurrentPosition();
-      head.move(body);
-      body.moveForward(headLastPosition);
-  
-      // check for collisions
-      if (apple.positionLeft === head.positionLeft && apple.positionTop === head.positionTop) {
-        $('#apple').remove();
-        apple = new Apple($('#board'));
-        console.log(body.nodes.length)
-        body.addNewNode(headLastPosition);
-      }
-    }
+  // Check if the snake is eating the apple every head.SPEED ms
+  function update() {
+    let headLastPosition = head.getCurrentPosition();
+    head.move(body);
+    body.moveForward(headLastPosition);
 
+    // check for collisions
+    if (
+      apple.positionLeft === head.positionLeft &&
+      apple.positionTop === head.positionTop
+    ) {
+      $("#apple").remove();
+      apple = new Apple($("#board"));
+      console.log(body.nodes.length);
+      body.addNewNode(headLastPosition);
+    }
+  }
 
   // IIFE on gameLoop func (command console)
-  // store all set timeout functionality in one fucntion for better performance and synchronicity of our game 
-  // current direction will change 1250 miliseconds 
+  // store all set timeout functionality in one fucntion for better performance and synchronicity of our game
+  // current direction will change 1250 miliseconds
   (function gameLoop() {
-    // 1. process key presses 
+    // 1. process key presses
 
     processInputs();
 
-
-    // 2. update the game world 
+    // 2. update the game world
     update();
 
-
-    // 3. draw the rest 
+    // 3. draw the rest
     // 3a. creating new apple will also draw it
     head.drawHead();
     body.draw();
 
-
-    // 4. start the game loop over 
-    setTimeout(gameLoop, head.SPEED)
+    // 4. start the game loop over
+    setTimeout(gameLoop, head.SPEED);
   })();
 
   // function checkRecursively(){
@@ -79,7 +78,7 @@ $(document).ready(function() {
   //     snakeBody.pop();
   //     snakeBody.unshift([head.positionLeft, head.positionTop]);
   //   }
-  //   //draw body 
+  //   //draw body
   //   //console.log('imhere', snakeBody, snakeBody.length)
   //   if(snakeBody.length > 1){
   //     for(let i = 1; i < snakeBody.length; i++){
@@ -90,6 +89,6 @@ $(document).ready(function() {
   //   setTimeout(checkRecursively, head.SPEED);
   // }
   // //invoke the recursive function
-  
+
   // checkRecursively();
 });
